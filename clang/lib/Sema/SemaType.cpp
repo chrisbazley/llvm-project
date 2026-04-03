545d52b954c96c9f85fefbfd28964ae77fcfc6f9
Christopher Bazley <cs99cjb@gmail.com>
2026-04-03 22:07:25 +0000

    Remove warning about optional return type

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -5028,14 +5028,6 @@ static TypeSourceInfo *GetFullTypeForDeclarator(TypeProcessingState &state,
         //   A volatile-qualified return type is deprecated
         if (T.isVolatileQualified() && S.getLangOpts().CPlusPlus20)
           S.Diag(DeclType.Loc, diag::warn_deprecated_volatile_return) << T;
-
-        // A function declarator shall not specify a return type that is
-        // optional-qualified.
-        if (T.isOptionalQualified()) {
-          S.Diag(DeclType.Loc,
-                  diag::err_typecheck_invalid_optional_return)
-              << T;
-        }
       }
 
       // Objective-C ARC ownership qualifiers are ignored on the function
